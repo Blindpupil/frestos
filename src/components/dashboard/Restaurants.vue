dec9971fef6ef422e269a212da540df7c704ca6a
Michaelxk <linares.michael@gmail.com>
2018-08-10 01:30:47 -0400

    add Nearby component in Dasboard/Restaurants

--- src/components/dashboard/Restaurants.vue
+++ src/components/dashboard/Restaurants.vue
@@ -2,6 +2,48 @@
   <div> 
       <h2>Content from restaurants component </h2>
       <p> {{ restaurants }}</p>
+     
+     <div>
+         <v-card height="200px" flat>
+    <div class="headline text-xs-center pa-5">
+      Active: {{ bottomNav }}
+    </div>
+    <v-bottom-nav
+      :active.sync="bottomNav"
+      :value="true"
+      absolute
+      color="transparent"
+    >
+      <v-btn
+        color="teal"
+        flat
+        value="recent"
+      >
+        <span>Recent</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn
+        color="teal"
+        flat
+        value="favorites"
+      >
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn
+        color="teal"
+        flat
+        value="nearby"
+      >
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-card>
+     </div>
+
   </div>
 </template>
 
@@ -15,7 +57,9 @@
     created() {
       this.$store.dispatch('setRestosRef', restosRef)
     },
-    components: {},
+    components: {
+       color: 'white'
+    },
     computed: mapGetters(['restaurants']),
     data() {
       return {
