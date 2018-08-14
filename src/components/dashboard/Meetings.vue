4fff2cb50aa4a5cc32c1845ad690576a61026965
Michaelxk <linares.michael@gmail.com>
2018-08-13 21:39:27 -0400

    update files

--- src/components/dashboard/Meetings.vue
+++ src/components/dashboard/Meetings.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
+   <div>
+      <h2>Content meetings</h2>
+  <v-layout justify-content-center>
     <div>
       <v-date-picker v-model="datepicker" :reactive="reactive"></v-date-picker>
-    </div>
+   
 
-    <div>
+
+    
       <v-time-picker v-model="timepicker" :reactive="reactive"></v-time-picker>
     </div>
+
+ 
+   </v-layout>
   </div>
 </template>
 
@@ -27,3 +33,4 @@
 <style scoped lang="scss">
 
 </style>
+<v-layout align-center justify-center row fill-height/>
