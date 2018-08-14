08f55f88eec3ef48c7cbd36895d7187f679d9db4
Michaelxk <linares.michael@gmail.com>
2018-08-14 01:40:30 -0400

    dealing with branch issue

--- src/components/dashboard/Restaurants.vue
+++ src/components/dashboard/Restaurants.vue
@@ -1,24 +1,5 @@
 <template>
   <div> 
-        <div>
-          <v-card>
-              <v-bottom-nav :active.sync="bottomNav" :value="true" absolute color="transparent">
-                  <v-btn color="teal" flat value="recent">
-                      <span>Recent</span>
-                      <v-icon>history</v-icon>
-                  </v-btn>  
-                    <v-btn color="teal" flat value="favorites">
-                        <span>Favorites</span>
-                        <v-icon>favorite</v-icon>
-                    </v-btn>
-                  <v-btn color="teal" flat value="nearby">
-                    <span>Nearby</span>
-                    <v-icon>place</v-icon>
-                  </v-btn>
-              </v-bottom-nav>
-          </v-card>
-        </div>
-
           <h2>Content from restaurants component </h2>
           <p> {{ restaurants }}</p>  
   </div>
